3ab4c7dbce96ddae28f0cae2bc9741c4729b6c30
Guillaume Martinez <guillaume.martinez.1804@gmail.com>
2025-07-14 10:57:46 +0200

    Remove exposed key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vzqktwawrrakutstvcsa.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6cWt0d2F3cnJha3V0c3R2Y3NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMjQwNTMsImV4cCI6MjA2NTkwMDA1M30.2IxHOoPuLqq1rKpRKyMWEpV5Mqp717SwEFM1wVJQmaE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
